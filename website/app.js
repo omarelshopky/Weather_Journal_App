9cf9325682b2a49117f18a7a0bf4380948947756
Omar Hesham <oommaarr.220022@gmail.com>
2021-06-17 01:13:25 +0200

    Add listener to add button

--- website/app.js
+++ website/app.js
@@ -1,8 +1,17 @@
-/*       Global Variables         */
+/***            Global Variables         ***/
 
-const apiUrl = 'api.openweathermap.org/data/2.5/weather?q=';
+const apiUrl = 'api.openweathermap.org/data/2.5/weather?id=';
 const apiKey = '&appid=53058c171c2fa87400423531cdf9bf51'; // Personal API Key for OpenWeatherMap API
 
+
+/***          Event listener to add function to existing HTML DOM element           ***/
+document.getElementById('add').addEventListener('click', addNewEntry);
+
+
+/* Function called by event listener */
+function addNewEntry(event) {
+    console.log("clicked");
+}
 
 
 // Create a new date instance dynamically with JS
