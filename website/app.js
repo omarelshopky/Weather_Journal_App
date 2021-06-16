a9cba5e7a66bd13805a46556776fc7470bad4939
Omar Hesham <oommaarr.220022@gmail.com>
2021-06-17 00:50:50 +0200

    Add API detail to client side

--- website/app.js
+++ website/app.js
@@ -1,4 +1,9 @@
-/* Global Variables */
+/*       Global Variables         */
+
+const apiUrl = 'api.openweathermap.org/data/2.5/weather?q=';
+const apiKey = '&appid=53058c171c2fa87400423531cdf9bf51'; // Personal API Key for OpenWeatherMap API
+
+
 
 // Create a new date instance dynamically with JS
 let d = new Date();
